2feddd3f6fbf7fb47f9b5913ebd17c2f93b32cc8
Jon Steele <jonwhsteele@gmail.com>
2023-02-23 16:00:10 -0800

    Added a heuristic function

--- functions.py
+++ functions.py
@@ -17,9 +17,25 @@ def cost(a, b):
     ]
     return cost[a][b]
 
-# heuristic returns the heuristic value between two city halls
+# heuristic returns the straight line distance between two city halls
+# This was calculated using google earth
 def heuristic(a, b):
-    return 0
+    straight_line_distance = [
+        [0, 7.8, 12.5, 10.6, 11.4, 21.5, 16.1, 21.8, 38.2, 62, 85, 121.8, 62.8], # Vancouver
+        [7.8, 0, 11.2, 11.8, 18.9, 21.7, 17, 26.4, 38.6, 61.6, 83.7, 118, 60], # North Vancouver
+        [12.5, 11.2, 0, 21.1, 21.6, 31.9, 27.3, 30.9, 49.3, 72.4, 94.8, 128.7, 71.9], # West Vancouver
+        [10.6, 11.8, 21.1, 0, 14.9, 10.5, 6.1, 18.8, 27.8, 51.6, 74.8, 111.9, 50.9], # Burnaby
+        [11.4, 18.9, 21.6, 14.9, 0, 21.3, 17.1, 10.5, 35.6, 60, 87, 125, 62.1], # Richmond
+        [21.5, 21.7, 31.9, 10.5, 21.3, 0, 4.6, 19.6, 17, 40.4, 65.2, 104.1, 41.17], # Surrey
+        [16.1, 17, 27.3, 6.1, 17.1, 4.6, 0, 17.3, 21.4, 45.5, 69.7, 108.3, 45.8], # New Westminister
+        [21.8, 26.4, 30.9, 18.8, 10.5, 19.6, 17.2, 0, 28.8, 52.9, 81.9, 121, 56.6], # Delta
+        [38.2, 38.6, 49.3, 27.8, 35.6, 17, 21.4, 28.8, 0, 24.7, 51.7, 93.4, 28.1], # Langley
+        [62, 61.6, 72.4, 51.6, 60, 40.4, 45.5, 52.9, 24.7, 0, 30, 74.1, 11.8], # Abbotsford
+        [85, 83.7, 94.8, 74.8, 87, 65.2, 69.7, 81.9, 51.7, 30, 0, 44.7, 23.6], # Chilliwack
+        [121.8, 118, 128.7, 111.9, 125, 104.1, 108.3, 121, 93.4, 74.1, 44.7, 0, 50], # Hope
+        [62.8, 60, 71.9, 50.9, 62.1, 41.17, 45.8, 56.6, 28.1, 11.8, 23.6, 50, 0] # Mission
+    ]
+    return straight_line_distance[a, b]
 
 # number_to_city converts an integer to a city name, as cities are stored as indices
 def number_to_city(x):
